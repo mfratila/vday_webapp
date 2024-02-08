7216149cbc0b600af6944aae417c151c2539de2a
Mihai Fratila <mihai.fratila01@gmail.com>
2024-02-08 16:44:52 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ import "./App.css";
 // Define the prop type for the Surprise component
 interface SurpriseProps {
   onClose: () => void;
+  imgLink : string;
 }
 
 // Surprise component to display the picture
-const Surprise: React.FC<SurpriseProps> = ({ onClose }) => {
+const Surprise: React.FC<SurpriseProps> = ({ onClose, imgLink }) => {
   return (
     <div className="surprise-container">
       <img
-        src="/maria.jpg"
-        alt="Surprise Photo"
+        src={imgLink}
+        alt="Photo"
         style={{ maxWidth: "100%" }}
       />
       <button onClick={onClose}>Close</button>
@@ -25,6 +26,7 @@ const Page: React.FC = () => {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const [showSurprise, setShowSurprise] = useState(false);
+  const [showHumping, setShowHumping] = useState(false);
   const yesButtonSize = noCount * 20 + 16;
 
   const handleNoClick = () => {
@@ -33,7 +35,13 @@ const Page: React.FC = () => {
 
   const handleSurpriseClick = () => {
     setShowSurprise(true);
+    setShowHumping(false);
   };
+
+  const handleHumpingClick = () => {
+    setShowHumping(true);
+    setShowSurprise(false);
+  }
 
   const getNoButtonText = () => {
     const phrases = [
@@ -62,11 +70,14 @@ const Page: React.FC = () => {
           <>
             <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
             <div className="text-container" style={{ color: "yellow" }}>
-              Yaaaaay!!!
+              Yaaaaay!!!💖💗
             </div>
             {/* Show the Surprise button after "Yes" is clicked */}
             <button onClick={handleSurpriseClick} className="surprise-button">
               Apasa aici pentru o surpriza 😉
+            </button>
+            <button onClick={handleHumpingClick} className="humping-button">
+              Asta o sa-ti fac cand te vad 😫
             </button>
           </>
         ) : (
@@ -99,7 +110,8 @@ const Page: React.FC = () => {
         )}
 
         {/* Show the Surprise component when showSurprise state is true */}
-        {showSurprise && <Surprise onClose={() => setShowSurprise(false)} />}
+        {showSurprise && <Surprise imgLink="/maria.jpg" onClose={() => setShowSurprise(false)} />}
+        {showHumping && <Surprise imgLink="/humping.gif" onClose={() => setShowHumping(false)} />}
       </div>
     </div>
   );
